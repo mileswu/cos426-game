274b09c34aef930f3f56ffa86f1d528c0436bb2e
Michael Bailey <mabstorm@yahoo.com>
2012-05-12 23:06:18 -0400

    added game audio and collision sound effect

--- src/game.cpp
+++ src/game.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include "game.h"
 #include "gl.h"
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <time.h>
 
 using namespace std;
+
+static sf::Music music;
+
 
 int main(int argc, char **argv) {
   CreateWindow();
@@ -15,6 +21,25 @@ int main(int argc, char **argv) {
   }
   fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
   
+  // http://offog.org/music/songs.html
+  if (!music.openFromFile("audio/hello-world.ogg")) {
+    printf("failed to find music\n");
+    return EXIT_FAILURE;
+  }
+
+  int milisec = 20; // length of time to sleep, in miliseconds
+  struct timespec ts = {0};
+  ts.tv_sec = 0;
+  ts.tv_nsec = milisec * 1000000L;
+
+  // Play the music
+  music.setLoop(true);
+  music.play();
+  for (float i = 0; i < 101; i++) {
+    music.setVolume(i);
+    nanosleep(&ts, (struct timespec *)NULL);
+  }
+
   glutMainLoop();
   
   return 0;
